fc45fd0eb31776e442c6819e6d15aaaff36683df
kartikmujmule <kartikmujmule7@gmail.com>
2025-10-16 11:51:08 +0530

    Add initial code for NewFeature class

---
+++ Newfile.java
@@ -0,0 +1,7 @@
+public class NewFeature {
+    public static void main(String[] args) {
+        System.out.println("Initial code for PBL.");
+        // ADD THIS LINE
+        System.out.println("This is the required modification!"); 
+    }
+}
